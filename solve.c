cbc94c1bbeb1c13decd54e66bd2de355d457f457
William Ambrozic <william.ambrozic@gmail.com>
2018-11-25 17:43:03 -0500

    Update solve.c

--- solve.c
+++ solve.c
@@ -14,6 +14,8 @@ The data indecies are based on their alphabetical index
 i.e.
 =>[A,B,C,D,E,F,G,H,I,J,K ,L ,M ,N ,O ]
 =>[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+
+This program is a brute-force approach
 *****/
 
 #include <stdio.h>
